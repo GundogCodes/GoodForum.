f56c3435f241812c39843bd4c03600b2b53379e7
Gunish <gunishsharma20@gmail.com>
2023-09-02 14:24:46 -0600

    added recentposts component for userPage

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,11 +1,12 @@
 import styles from './UserPage.module.scss'
 import HeadsUp from '../../components/HeadsUp/HeadsUp'
+import RecentPosts from '../../components/RecentPosts/RecentPosts'
 export default function UserPage(){
     return(
-
         <div className={styles.UserPage}>
             <h1></h1>
         <HeadsUp />
+        <RecentPosts />
         </div>
 )
 }
