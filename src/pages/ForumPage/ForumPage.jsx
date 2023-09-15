aa5e8992b8b6f6779e31d9303369a6c0132d4341
Gunish <gunishsharma20@gmail.com>
2023-09-14 21:44:28 -0600

    didnt save file

--- src/pages/ForumPage/ForumPage.jsx
+++ src/pages/ForumPage/ForumPage.jsx
@@ -1,7 +1,7 @@
 import styles from './ForumPage.module.scss'
 import Footer from '../../components/Footer/Footer'
 
-export default function ForumPage(){
+export default function ForumPage({user,setUser}){
     return(
 
         <div className={styles.ChatsPageForumPage}>
