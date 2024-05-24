a7d1cd5683b58b872ac41fb713d5db371ea750dc
Gunish <gunishsharma20@gmail.com>
2024-05-23 22:41:14 -0600

    bunch of changes

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -4,13 +4,16 @@ import UserAside from "../../components/UserAside/UserAside";
 import Footer from "../../components/Footer/Footer";
 import { useState, useEffect } from "react";
 import PostModal from "../../components/PostModal/PostModal";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import * as usersAPIs from "../../../utilities/users-api.cjs";
 import Post from "../../components/Post/Post";
-
+import { useNavigate } from "react-router-dom";
+//import * as userService from "../../../utilities/users-api.cjs";
+import axios from "axios";
 export default function UserPage({ user, setUser }) {
   /********************************************** VARIABLES **********************************************/
   const { id } = useParams();
+  const navigate = useNavigate();
   const [showModal, setShowModal] = useState(false);
   //   useEffect(() => {
   //     (async () => {
@@ -40,6 +43,56 @@ export default function UserPage({ user, setUser }) {
       }
     })();
   }, []);
+  /******************************************** States ********************************************/
+  const [file, setFile] = useState();
+  const [showUploadForm, setShowUploadForm] = useState(false);
+
+  /******************************************** Handle States ********************************************/
+  function showForm() {
+    setShowUploadForm(!showUploadForm);
+  }
+  /******************************************** Control Panel Functions ********************************************/
+  function handleControlPanel(e) {
+    if (!user) {
+      alert("Login to Continue");
+    }
+    const button = e.target.name;
+    if (user && button === "friends") {
+      navigate("/friends");
+    } else if (user && button === "settings") {
+      navigate("/settings");
+    }
+  }
+  function setShow() {
+    if (!user) {
+      alert("Login to Continue");
+      navigate("/login");
+    }
+    setShowModal(true);
+    console.log(showUploadForm);
+  }
+
+  /******************************************** Uploading Profile Images Async Functions ********************************************/
+
+  async function submit(e) {
+    e.preventDefault();
+    console.log("file", file.name);
+    const formData = new FormData();
+    formData.append("profilePic", file);
+    const result = await axios.post("/api/profilePic", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    console.log(result.data);
+    try {
+      const updatedUser = await userService.updateUserInfo(user._id, {
+        profileImage: file.name,
+      });
+      console.log("updatedUser", updatedUser);
+      setUser(updatedUser);
+    } catch (error) {
+      console.log({ error: error });
+    }
+  }
   console.log(allUserPost);
   return (
     <div className={styles.UserPage}>
@@ -54,7 +107,17 @@ export default function UserPage({ user, setUser }) {
         <></>
       )}
       <div className={styles.userAside}>
-        <h1>{user.username}</h1>
+        {user ? (
+          <h1>{user.username}</h1>
+        ) : (
+          <h1
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            Login
+          </h1>
+        )}
         {user ? (
           <img
             id={styles.profilePic}
@@ -66,8 +129,34 @@ export default function UserPage({ user, setUser }) {
             src={`/src/assets/userFunc/profileImage.png`}
           />
         )}
-
-        <h2>Edit</h2>
+        {user && user.profileImage ? (
+          <p onClick={showForm} className={styles.uploadPic}>
+            Update Profile Pic
+          </p>
+        ) : (
+          <p onClick={showForm} className={styles.uploadPic}>
+            Upload Profile Pic
+          </p>
+        )}
+        {showUploadForm ? (
+          <form onSubmit={submit}>
+            <input
+              onChange={(e) => setFile(e.target.files[0])}
+              type="file"
+              accept="image/*"
+            />
+            <button type="submit">Upload!</button>
+          </form>
+        ) : (
+          <></>
+        )}
+        {/* <h2
+          onClick={() => {
+            setShowUploadForm(true);
+          }}
+        >
+          Edit
+        </h2> */}
         {user ? (
           <div className={styles.bio}>
             <h2>{user.bio}</h2>
@@ -78,9 +167,26 @@ export default function UserPage({ user, setUser }) {
           </div>
         )}
         <div className={styles.functions}>
-          <h3>friends</h3>
-          <h3>settings</h3>
-          <h3>+</h3>
+          <Link to="/friends">
+            <img
+              className={styles.userFuncs}
+              src="/src/assets/userFunc/friends.png"
+            />
+          </Link>
+          <Link to="/settings">
+            <img
+              className={styles.userFuncs}
+              src="/src/assets/userFunc/settings.png"
+            />
+          </Link>
+          <h4
+            className={styles.userFuncs}
+            onClick={() => {
+              setShowModal(true);
+            }}
+          >
+            +
+          </h4>
         </div>
       </div>
       <div className={styles.userPosts}>
